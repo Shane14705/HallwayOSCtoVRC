11167cf6f893ae6900c99b76d582bee15ef233cc
Shane McKelvey <psmnhb@umsystem.edu>
2022-06-21 14:37:26 -0500

    Created VRCClient class

---
+++ HallwayOSCtoVRC/VRCClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HallwayOSCtoVRC
+{
+    /*This class will act as a representation of the VRChat client. It will be responsible for
+     
+     */
+    internal class VRCClient
+    {
+    }
+}
