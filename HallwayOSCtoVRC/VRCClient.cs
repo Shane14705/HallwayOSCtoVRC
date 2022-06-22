df831b27471fa63edf94ab509d629567eb739b3a
Shane McKelvey <psmnhb@umsystem.edu>
2022-06-22 16:29:22 -0500

    Added basic listening loop and added stub for HandleAviChange method

--- HallwayOSCtoVRC/VRCClient.cs
+++ HallwayOSCtoVRC/VRCClient.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using CoreOSC;
+using CoreOSC.IO;
 
 namespace HallwayOSCtoVRC
 {
@@ -13,9 +15,13 @@ namespace HallwayOSCtoVRC
     internal class VrcClient
     {
 
+        //Connection Parameters
         private string m_address = "127.0.0.1";
         private int m_receivePort = 9001;
         private int m_sendPort = 9000;
+        private bool m_listening = true;
+
+        private string m_currentAviID = null;
 
         /*
          * Put logic for changing connections when addresses/ports are changed in these setters
@@ -37,5 +43,45 @@ namespace HallwayOSCtoVRC
             get => m_sendPort;
             set => m_sendPort = value;
         }
+
+        //Initiates a VrcClient and connects it to the user's VRChat instance using the specified connection parameters
+        public VrcClient(string address, int receivePort, int sendPort) {
+            m_address = address;
+            m_receivePort = receivePort;
+            m_sendPort = sendPort;
+        }
+
+        //Initiates a VrcClient and connects it to the user's VRChat instance with default connection parameters
+        public VrcClient()
+        {
+            m_address = "127.0.0.1";
+            m_receivePort = 9001;
+            m_sendPort = 9000;
+        }
+
+        private async void ListenLoop()
+        {
+            while (m_listening)
+            {
+                using (UdpClient client = new UdpClient(m_address, m_receivePort))
+                {
+                    OscMessage response = await client.ReceiveMessageAsync();
+
+                    if (response.Address.Value == "/avatar/change")
+                    {
+                        m_currentAviID = (string)(response.Arguments.ElementAt(0));
+                        HandleAviChange();
+                    }
+                }
+            }
+            
+        }
+
+        private async Task HandleAviChange()
+        {
+            throw new NotImplementedException();
+        }
     }
+
+
 }
