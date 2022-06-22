d6076cc114d2c5ec4e19ac22880090511b4e70c6
Shane McKelvey <psmnhb@umsystem.edu>
2022-06-22 14:01:07 -0500

    Added default connection parameters to VRCClient Class

--- HallwayOSCtoVRC/VRCClient.cs
+++ HallwayOSCtoVRC/VRCClient.cs
@@ -10,7 +10,32 @@ namespace HallwayOSCtoVRC
     /*This class will act as a representation of the VRChat client. It will be responsible for
      
      */
-    internal class VRCClient
+    internal class VrcClient
     {
+
+        private string m_address = "127.0.0.1";
+        private int m_receivePort = 9001;
+        private int m_sendPort = 9000;
+
+        /*
+         * Put logic for changing connections when addresses/ports are changed in these setters
+         */
+        public string Address
+        {
+            get => m_address;
+            set => m_address = value;
+        }
+
+        public int ReceivePort
+        {
+            get => m_receivePort;
+            set => m_receivePort = value;
+        }
+
+        public int SendPort
+        {
+            get => m_sendPort;
+            set => m_sendPort = value;
+        }
     }
 }
